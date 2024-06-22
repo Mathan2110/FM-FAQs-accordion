39b6938dc7dcfce3068c73897ab8325336d969a7
Mathan5410 <maddy5410y@gmail.com>
2024-06-22 13:43:41 +0530

    project upload

---
+++ script.js
@@ -0,0 +1,77 @@
+function open1(){
+    var ans=document.querySelector(".question1 .answer");
+    var plus_image=document.querySelector(".question1 .plus");
+    var minus_image=document.querySelector(".question1 .minus");
+    ans.style.display="block";
+    plus_image.style.display="none"
+    minus_image.style.display="block"
+}
+
+function open2(){
+    var ans=document.querySelector(".question2 .answer");
+    var plus_image=document.querySelector(".question2 .plus");
+    var minus_image=document.querySelector(".question2 .minus");
+    ans.style.display="block";
+    plus_image.style.display="none"
+    minus_image.style.display="block"
+
+}
+
+function open3(){
+    var ans=document.querySelector(".question3 .answer");
+    var plus_image=document.querySelector(".question3 .plus");
+    var minus_image=document.querySelector(".question3 .minus");
+    ans.style.display="block";
+    plus_image.style.display="none"
+    minus_image.style.display="block"
+
+}
+
+function open4(){
+    var ans=document.querySelector(".question4 .answer");
+    var plus_image=document.querySelector(".question4 .plus");
+    var minus_image=document.querySelector(".question4 .minus");
+    ans.style.display="block";
+    plus_image.style.display="none"
+    minus_image.style.display="block"
+
+}
+
+function close1(){
+    var ans=document.querySelector(".question1 .answer");
+    var plus_image=document.querySelector(".question1 .plus");
+    var minus_image=document.querySelector(".question1 .minus");
+    ans.style.display="none";
+    plus_image.style.display="block"
+    minus_image.style.display="none"
+}
+
+function close2(){
+    var ans=document.querySelector(".question2 .answer");
+    var plus_image=document.querySelector(".question2 .plus");
+    var minus_image=document.querySelector(".question2 .minus");
+    ans.style.display="none";
+    plus_image.style.display="block"
+    minus_image.style.display="none"
+
+}
+
+function close3(){
+    var ans=document.querySelector(".question3 .answer");
+    var plus_image=document.querySelector(".question3 .plus");
+    var minus_image=document.querySelector(".question3 .minus");
+    ans.style.display="none";
+    plus_image.style.display="block"
+    minus_image.style.display="none"
+
+}
+
+function close4(){
+    var ans=document.querySelector(".question4 .answer");
+    var plus_image=document.querySelector(".question4 .plus");
+    var minus_image=document.querySelector(".question4 .minus");
+    ans.style.display="none";
+    plus_image.style.display="block"
+    minus_image.style.display="none"
+
+}
